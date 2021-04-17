0ac6402fea2e78de644b6ea935e62e75e2db47e1
sven-eliasen <jo.pettersen@pm.me>
2021-04-17 08:30:08 -0700

    added Ctr+C support to stop main loop

---
+++ libinv-d/daily_run.h
@@ -0,0 +1,10 @@
+//
+// Created by user0 on 4/17/21.
+//
+
+#ifndef INVENTORY_DAILY_DAILY_RUN_H
+#define INVENTORY_DAILY_DAILY_RUN_H
+
+void daily_run();
+
+#endif //INVENTORY_DAILY_DAILY_RUN_H
--- main.c
+++ main.c
@@ -1,30 +1,12 @@
 #include <stdio.h>
 #include <time.h>
 #include <unistd.h>
+#include "libinv-d/daily_run.h"
 
 int main() {
 
     // main loop
-    while (1){
-
-        // get time in hour and minute
-        time_t now = time(NULL);
-        struct tm *tm_struct = localtime(&now);
-        int hour = tm_struct->tm_hour;
-        int minutes = tm_struct->tm_min;
-
-        // backup time
-        if ((hour == 23 || hour == 11) && (minutes == 59)){
-
-            // to work only once in a minute
-            sleep(60);
-
-            // the work todo
-            FILE* f = fopen("test.txt", "a");
-            fprintf(f, "%d:%d\n", hour, minutes);
-            fclose(f);
-        }
-    }
+    daily_run();
 
     return 0;
 }
